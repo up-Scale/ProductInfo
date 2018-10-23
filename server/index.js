6a946390b9323bc174afbe40560b8a05866fb466
John Sangiolo <johnsangiolo@gmail.com>
2018-10-23 14:45:11 -0400

    updated index.jsx to render initial component to localhost

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var parser = require('body-parser');
 
 let app = express();
 app.use(parser.json());
-app.use(express.static(__dirname + '../react-client/dist'));
+app.use(express.static(__dirname + '/../react-client/dist'));
 
 let port = 3001;
 app.listen(port, () => {
