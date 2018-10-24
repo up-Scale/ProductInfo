a138281e88344ffe64dfffb4009c3740ba30e23f
John Sangiolo <johnsangiolo@gmail.com>
2018-10-24 10:30:36 -0400

    added constructor and state to index.jsx

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class ProductInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reminder: false
+    }
+  }
+
   render() {
     return (
       <div>Hello, Future Product Component Info!</div>
