e50584e8e64063ffd4ac1949317eea4274de91a6
John Sangiolo <johnsangiolo@gmail.com>
2018-10-30 16:02:56 -0400

    refactored and applied inital styling to Pricing component

--- react-client/src/components/Pricing.jsx
+++ react-client/src/components/Pricing.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Price from './styled-components/Price.jsx';
+import SalePrice from './styled-components/SalePrice.jsx';
 
 const Pricing = ({prices}) => {
+  
+  const convertNumber = (x) => {
+    if (x) return x.toLocaleString();
+  }
+  
   return(
-    <div>{prices.price} {prices.sale_price}</div>
+    <div>
+      {prices.sale_price > 0 
+        ? <div><SalePrice>{convertNumber(prices.sale_price)}</SalePrice> <Price>{convertNumber(prices.price.toLocaleString())}</Price></div> 
+        : <SalePrice>{convertNumber(prices.price)}</SalePrice> }
+    </div>
   )
 }
 
