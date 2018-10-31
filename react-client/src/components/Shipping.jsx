a4f964b08fafd5411be5fb0fbb3eb3062296123d
John Sangiolo <johnsangiolo@gmail.com>
2018-10-30 20:16:10 -0400

    refactored and styled time remaining data point

--- react-client/src/components/Shipping.jsx
+++ react-client/src/components/Shipping.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import TimeLeft from './styled-components/TimeLeft.jsx';
 
 const Shipping = ({shipping}) => {
 
   const getTimeDiff = (diff) => {
     let days = Math.floor(diff / 86400)
     let hours = Math.floor(diff / 3600) % 24
-    return `${days} days ${hours} hours remaining `
+
+    if (days > 0) return <TimeLeft style={{color:"rgb(132, 148, 147)"}}>{`${days} days left`}</TimeLeft>
+    else return <TimeLeft style={{color:"rgb(255, 114, 118)"}}>{`${hours} hours left`}</TimeLeft>
   }
 
   return(
-    <div>{getTimeDiff((new Date(shipping.time * 1000) - new Date()) / 1000)} 
+    <div>
+      {getTimeDiff((new Date(shipping.time * 1000) - new Date()) / 1000)}  
       {`${shipping.units_sold} sold `} 
       {shipping.shipping_option}
     </div>
