fe1d2177bcd38f7550fb5b2c8124853eaa1057d4
Kenni Silverio <ksilverio31@gmail.com>
2018-11-08 16:20:57 -0500

    set up schemas and seed file for 100000 sets of data using sequelize, going to test run the seed file to see if it succeeds, if it does then need to refactor routes in server

--- db/index.js
+++ db/index.js
@@ -1,11 +1,147 @@
-var db = require('mysql');
-//var pass = require('../config.js');
+//        ORIGINAL
 
-const connection = db.createConnection({
-  host: 'fec-product-info.clj3efkrub7r.us-east-2.rds.amazonaws.com',
-  user: 'johnsangiolo',
-  password: 'Seniorphase014',
-  database: 'product_info'
+
+// var db = require('mysql');
+// //var pass = require('../config.js');
+
+// const connection = db.createConnection({
+//   host: 'fec-product-info.clj3efkrub7r.us-east-2.rds.amazonaws.com',
+//   user: 'johnsangiolo',
+//   password: 'Seniorphase014',
+//   database: 'product_info'
+// });
+
+// module.exports.connection = connection;
+
+
+//         POSTGRESQL
+
+
+// const { Pool, Client } = require('pg')
+// var conString = "postgres://kennisilverio:nopass@localhost:3001/productInfo";
+
+// const pool = new Pool({
+//   connectionString: conString,
+//   max: 20,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 2000,
+// })
+
+// pool.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   pool.end()
+// });
+
+// const client = new Client({
+//   connectionString: conString,
+//   max: 20,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 2000,
+// })
+
+// client.connect()
+
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   client.end()
+// })
+
+// const text = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
+// const values = ['brianc', 'brian.m.carlson@gmail.com']
+
+// client.query(text, values)
+//   .then(res => {
+//     console.log(res.rows[0])
+//     // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+//   })
+//   .catch(e => console.error(e.stack))
+
+// const fetchItem = {
+//   // give the query a unique name
+//   name: 'fetch-item',
+//   text: 'SELECT * FROM items WHERE id = $1',
+//   values: [1]
+// }
+
+// client.query(fetchItem)
+//       .then(res => console.log(res.rows[0]))
+//       .catch(e => console.error(e.stack))
+
+
+
+// module.exports.connection = client;
+
+
+
+//    SEQUELIZE
+
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('productInfo', 'kennisilverio', '', {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+})
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+const Item = sequelize.define('item', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING, 
+  },
+  price: {
+    type: Sequelize.DECIMAL,
+  }, 
+  sale_price: {
+    type: Sequelize.DECIMAL,
+  }, 
+  average_score: {
+    type: Sequelize.INTEGER,
+    max: 5,
+  }, 
+  deal_ends: {
+    type: Sequelize.DATE,
+  }, 
+  units_sold: {
+    type: Sequelize.INTEGER,
+  }, 
+  shipping_option: {
+    type: Sequelize.STRING,
+  }, 
+  drop_count: {
+    type: Sequelize.INTEGER,
+  }
 });
 
-module.exports.connection = connection;
+const Category = sequelize.define('category', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+  }
+})
+
+  // REDIS
+module.exports = {
+  Item : Item,
+  Category: Category,
+}
+
---
+++ db/seed.js
@@ -0,0 +1,68 @@
+const {Item, Category} = require('./index.js')
+const faker = require('faker')
+
+// insert into items (
+//     name, price, sale_price, number_of_reviews, average_score, deal_ends, units_sold, shipping_option, banner_image_id, drop_count
+// )
+// select
+//     md5(i::text)),
+//     TO_NUMBER(md5(random()::text)),
+//     md5(random()::text),
+//     left(md5(random()::text), 4)
+// from generate_series(1, 10000000) s(i)
+
+const genProductName = faker.commerce.productName();
+const genPrice = faker.commerce.price();
+const genSalesPrice = faker.commerce.price() < genPrice ? faker.commerce.price() : genPrice;
+const genAverageScore = faker.random.number();
+const genDealEndDate = faker.date.soon();
+const genUnitsSold = faker.random.number();
+const genShippingOpt = "Free Shipping to USA";
+const genDropCount = 1;
+// const insertItem = "insert into items(name, price, sale_price, average_score, deal_ends, units_sold, shipping_option, drop_count) values($1, $2, $3, $4, $5, $6, $7, $8)"
+// const insertItemValues = [genProductName, genPrice, genSalesPrice, genAverageScore, genDealEndDate, genUnitsSold, genShippingOpt, genDropCount]
+const genProduct = faker.commerce.product();
+// const insertCat = "insert into categories(name) values($1)"
+// const insertCatValues = [genProduct];
+
+// for (var i = 0; i < 100000; i++){
+//     db.connection.query(insertItem, insertItemValues)
+//                  .catch(err => console.log(err, "error in seed file items"))
+//     db.connection.query(insertCat, insertCatValues)
+//                  .catch(err => console.log(err, "error in seed file categories"))
+// }
+
+Item.sync({force: true})
+    .then(() => {
+        for (var i = 0, p = Promise.resolve(); i < 100000; i++){
+            p = p.then(_ => new Promise(resolve => {
+                Item.create({
+                    name: genProductName,
+                    price: genPrice,
+                    sale_price: genSalesPrice,
+                    average_score: genAverageScore,
+                    deal_ends: genDealEndDate,
+                    units_sold: genUnitsSold,
+                    shipping_option: genShippingOpt,
+                    drop_count: genDropCount,
+                })
+                resolve();
+            }))
+            .catch(err => console.log(err, "err @ Item create"))
+        }
+    })
+    .catch(err => console.log(err, "err @ Item sync"))
+
+Category.sync({force: true})
+        .then(() => {
+            for (var i = 0, p = Promise.resolve(); i < 100000; i++){
+                p = p.then(_ => new Promise(resolve => {
+                    Category.create({
+                        name: genProduct,
+                    })
+                    resolve();
+                }))
+                .catch(err => console.log(err, "err @ Item create"))
+            }
+        })
+        .catch(err => console.log(err, "err @ Item sync"))
